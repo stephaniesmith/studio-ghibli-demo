45faaf54bed66f0b020358492a74941a092d73f5
Stephanie <stephanie.smith@leanpath.com>
2021-02-11 19:10:46 -0800

    adding images

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,10 @@ import { HomeComponent } from './home/home.component';
 const routes: Routes = [
   {
     path: '',
+    redirectTo: 'home'
+  },
+  {
+    path: 'home',
     component: HomeComponent
   },
   {
