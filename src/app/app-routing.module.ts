6460ca4c08e2d8249c21cc977846eab8ba2640b6
Stephanie <stephanie.smith@leanpath.com>
2021-02-11 19:24:03 -0800

    getting nav correct

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,11 @@ import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
-  // {
-  //   path: '',
-  //   redirectTo: 'home'
-  // },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
   {
     path: 'home',
     component: HomeComponent
