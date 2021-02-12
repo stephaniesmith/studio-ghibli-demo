a0315cf41f4be094472f506cef78bd4b55c01419
Stephanie <stephanie.smith@leanpath.com>
2021-02-12 12:37:27 -0800

    updating app

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('demo-test app is running!');
+    expect(page.getTitleText()).toEqual('studio-ghibli-demo app is running!');
   });
 
   afterEach(async () => {
